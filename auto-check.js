d629540a4cbbfbab28f2ebc0003c88127950ba7e
Vitalina Kovbasiuk <101979968+VitalinaKovbasiuk@users.noreply.github.com>
2022-07-23 18:14:01 +0300

    Автоперевірка 40-41

--- auto-check.js
+++ auto-check.js
@@ -606,7 +606,17 @@
 
 /* Завдання 40 автоперевірка
  */
-
+// ЗАДАЧА: ОНОВЛЮЄМО ЗІЛЛЯ
+// Доповни метод updatePotionName(oldName, newName) таким чином, 
+// щоб він оновлював назву зілля з oldName на newName в масиві зілля у властивості potions.
+// const atTheOldToad = {
+//     potions: ["Speed potion", "Dragon breath", "Stone skin"],
+//     updatePotionName(oldName, newName) {
+//   const name = this.potions.indexOf(oldName)
+//   this.potions.splice(name, 1, newName);
+//     },
+//   };
+  
 
 
 
@@ -614,3 +624,88 @@
 
 /* Завдання 41 автоперевірка
  */
+// ЗАДАЧА: РОЗШИРЮЄМО ІНВЕНТАР
+// Замовниця хоче, щоб кожне зілля було представлено не тільки ім'ям, але й ціною, 
+// а в майбутньому, можливо, й іншими характеристиками. 
+// Тому зараз у властивості potions буде зберігатися масив об'єктів з наступними властивостями.
+
+// {
+//   name: "Dragon breath",
+//   price: 700
+// }
+// Виконай рефакторинг методів об'єкта atTheOldToad таким чином, щоб вони працювали не з масивом рядків, а з масивом об'єктів.
+
+// getPotions() - метод для отримання всього зілля. Повертає значення властивості potions.
+// addPotion(newPotion) - додає зілля newPotion (вже об'єкт) в масив у властивості potions, 
+// але тільки, якщо такого зілля ще немає в інвентарі. В іншому випадку повертається рядок.
+// removePotion(potionName) - видаляє об'єкт зілля з ім'ям potionName з масиву у властивості potions.
+// updatePotionName(oldName, newName) - оновлює властивість name об'єкта-зілля з назвою oldName на newName в масиві potions.
+//!!!before:
+// const atTheOldToad = {
+//     potions: [
+//       { name: "Speed potion", price: 460 },
+//       { name: "Dragon breath", price: 780 },
+//       { name: "Stone skin", price: 520 },
+//     ],
+//     getPotions() {
+//       return this.potions;
+//     },
+//     addPotion(newPotion) {
+//       if (this.potions.includes(newPotion)) {
+//         return `Error! Potion ${newPotion} is already in your inventory!`;
+//       }
+//       this.potions.push(newPotion);
+//     },
+//     removePotion(potionName) {
+//       const potionIndex = this.potions.indexOf(potionName);
+//       if (potionIndex === -1) {
+//         return `Potion ${potionName} is not in inventory!`;
+//       }
+//       this.potions.splice(potionIndex, 1);
+//     },
+//     updatePotionName(oldName, newName) {
+//       const potionIndex = this.potions.indexOf(oldName);
+//       if (potionIndex === -1) {
+//         return `Potion ${oldName} is not in inventory!`;
+//       }
+//       this.potions.splice(potionIndex, 1, newName);
+//     },
+//   };
+//!!after:
+// const atTheOldToad = {
+//     potions: [
+//       { name: "Speed potion", price: 460 },
+//       { name: "Dragon breath", price: 780 },
+//       { name: "Stone skin", price: 520 },
+//     ],
+//     getPotions() {
+//       return this.potions;
+//     },
+//     addPotion(newPotion) {
+//       for (const potion of this.potions) {
+//         if (potion.name === newPotion.name) {
+//           return `Error! Potion ${newPotion.name} is already in your inventory!`;
+//         }
+//       }
+//       this.potions.push(newPotion);
+//     },
+//     removePotion(potionName) {
+//       for (const potion of this.potions) {
+//         if (potion.name === potionName) {
+//           const potionIndex = this.potions.indexOf(potion);
+//           if (potionIndex === -1) {
+//             return `Potion ${potionName} is not in inventory!`;
+//           }
+//           this.potions.splice(potionIndex, 1);
+//         }
+//       }
+//     },
+//     updatePotionName(oldName, newName) {
+//       for (const potion of this.potions) {
+//         if (potion.name === oldName) {
+//           potion.name = newName;
+//         }
+//       }
+//     },
+//   }; 
+  
